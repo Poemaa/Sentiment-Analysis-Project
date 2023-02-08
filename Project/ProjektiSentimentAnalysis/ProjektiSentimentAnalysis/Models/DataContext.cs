6b2c6e694aa3c7268f8b16f84fed5620cfc27d3b
Dije Ferati <df52387@ubt-uni.net>
2023-02-09 00:16:12 +0100

    updates

---
+++ Project/ProjektiSentimentAnalysis/ProjektiSentimentAnalysis/Models/DataContext.cs
@@ -0,0 +1,49 @@
+﻿
+//using ASP.NETCoreIdentityCustom.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjektiSentimentAnalysis.Areas.Identity;
+
+
+namespace ProjektiSentimentAnalysis.Models
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+                : base(options)
+        { }
+
+        public DbSet<Instituti> Institutis { get; set; }
+        public DbSet<Fakulteti> Fakultetis { get; set; }
+        public DbSet<Feedback> Feedbakcs { get; set; }
+        public DbSet<Infk> Infks { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+        }
+    }
+
+    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(u => u.Emri).HasMaxLength(255);
+            builder.Property(u => u.Mbiemri).HasMaxLength(255);
+        }
+    }
+
+        //  public class DataContext
+        //  {}
+    }
+
--- Project/Sentiment_Analysis_Project/Sentiment_Analysis_Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Sentiment_Analysis_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
